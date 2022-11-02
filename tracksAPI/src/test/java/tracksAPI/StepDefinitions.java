7c7edc0475c78f88708aa8687af9307220d7d549
Max Coldrick <maxcoldrick@gmail.com>
2022-11-02 15:44:38 +0000

    add automated scenarios and skeleton step definitions

--- tracksAPI/src/test/java/tracksAPI/StepDefinitions.java
+++ tracksAPI/src/test/java/tracksAPI/StepDefinitions.java
@@ -1,21 +1,61 @@
 package tracksAPI;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class StepDefinitions {
 
-    @Given("an example scenario")
-    public void anExampleScenario() {
+    @Given("an authenticated user")
+    public void anAuthenticatedUser() {
+        // Purposefully left blank as this endpoint has no authentication
     }
 
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
+    @When("a GET request is sent to {string}")
+    public void aGETRequestIsSentTo(String url) {
+
     }
 
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
+    @Then("the response code is {int}")
+    public void theResponseCodeIs(int targetResponseCode) {
     }
 
+    @And("the response time is below {int} second")
+    public void theResponseTimeIsLowerThanSecond(int targetResponseTime) {
+    }
+
+    @Then("the ID field is never null for all items")
+    public void theIDFieldIsNeverNullForAllItems() {
+    }
+
+    @And("the ID field is never empty for all items")
+    public void theIDFieldIsNeverEmptyForAllItems() {
+    }
+
+    @And("the segment_type field is {string} for all items")
+    public void theSegment_typeFieldIsForAllItems(String arg0) {
+    }
+
+    @Then("the primary title list is never null for all items")
+    public void thePrimaryTitleListIsNeverNullForAllItems() {
+    }
+
+    @And("the primary title list is never empty for all items")
+    public void thePrimaryTitleListIsNeverEmptyForAllItems() {
+    }
+
+    @Then("only one track reports true for now_playing")
+    public void onlyOneTrackReportsTrueForNow_playing() {
+
+    }
+
+    @Then("the response header has a valid date")
+    public void theResponseHeaderHasAValidDate() {
+    }
 }
---
+++ tracksAPI/src/test/resources/tracksAPI/tracks.feature
@@ -0,0 +1,36 @@
+Feature: An example
+
+  @automated
+  Scenario: Correct response code
+    Given an authenticated user
+    When a GET request is sent to "https://testapi.io/api/ottplatform/media"
+    Then the response code is 200
+    And the response time is below 1 second
+
+  @automated
+  Scenario: Validate ID field
+    Given an authenticated user
+    When a GET request is sent to "https://testapi.io/api/ottplatform/media"
+    Then the ID field is never null for all items
+    And the ID field is never empty for all items
+    And the segment_type field is "music" for all items
+
+  @automated
+  Scenario: Validate primary title list
+    Given an authenticated user
+    When a GET request is sent to "https://testapi.io/api/ottplatform/media"
+    Then the primary title list is never null for all items
+    And the primary title list is never empty for all items
+
+  @automated
+  Scenario: Now playing only reports true for one track
+    Given an authenticated user
+    When a GET request is sent to "https://testapi.io/api/ottplatform/media"
+    Then only one track reports true for now_playing
+
+  @automated
+  Scenario: Validate response header date
+    Given an authenticated user
+    When a GET request is sent to "https://testapi.io/api/ottplatform/media"
+    Then the response header has a valid date
+
